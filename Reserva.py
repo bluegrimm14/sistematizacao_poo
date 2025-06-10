5270a56ec384f6848f1d97d1e7f9fc85d15eecd3
Terry Roger <terryrogerps@gmail.com>
2025-06-10 18:33:17 -0300

    feat: Create Class Hotel and add getters methods in Reserva Class

--- Reserva.py
+++ Reserva.py
@@ -3,4 +3,11 @@ from Quarto import Quarto
 
 class Reserva:
     def __init__(self, hospede: Hospede, quarto: Quarto):
-        pass
+        self._hospede = hospede
+        self._quarto = quarto
+
+    def get_hospede(self):
+        return self._hospede
+    
+    def get_quarto(self):
+        return self._quarto
